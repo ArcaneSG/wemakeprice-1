a44673bf1f4cb4ea2035936beeaadda510333581
Eunbyul <iks1614@gmail.com>
2020-04-18 20:44:59 +0900

    index.html 화면 추가

--- src/main/java/com/keb/controller/MainController.java
+++ src/main/java/com/keb/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.keb.controller;
 
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.keb.converter.ConverterType;
 import com.keb.converter.OnlyTextConverter;
 import com.keb.converter.TagRemoveConverter;
+import com.keb.util.CharacterUtil;
 import com.keb.util.HttpConnectionUtil;
  
 @Controller
@@ -18,16 +23,30 @@ public class MainController {
     }
     
     @RequestMapping("/url")
-    public @ResponseBody String url(String url, String type) throws Exception {
-    	url = (url == null) ? "https://www.naver.com" : url;
+    public @ResponseBody Map<String, Object> url(String url, String type, String bundle) throws Exception {
+    	url = (url == null) ? "https://www.naver.com" : URLDecoder.decode(url, "UTF-8");
     	type = (type == null) ? "ONLY_TEXT" : type;
+    	bundle = (bundle == null) ? "1" : bundle;
     	//enum에 없을 경우 에러처리해야함
     	
+//    	System.out.println(url); //로그에 남길 것 url, 결과 길이
+    	String str = "";
     	if(ConverterType.TAG_REMOVE.equals(ConverterType.valueOf(type))) {
-    		return new TagRemoveConverter().convert(HttpConnectionUtil.httpConnection(url));
+    		str = new TagRemoveConverter().convert(HttpConnectionUtil.httpConnection(url));
     	}
     	
-    	return new OnlyTextConverter().convert(HttpConnectionUtil.httpConnection(url));
+    	str = new OnlyTextConverter().convert(HttpConnectionUtil.httpConnection(url));
+    	
+    	str = CharacterUtil.sort(str);
+    	
+    	//0으로 나눌 경우 에러처리
+    	Map<String, Object> model = new HashMap<>();
+    	int cutIndex = str.length() - str.length()%Integer.valueOf(bundle);
+    	model.put("quotient", str.substring(0, cutIndex));
+    	model.put("rest", str.substring(cutIndex));
+    	model.put("url", url);
+    	
+    	return model;
     	
     }
     
