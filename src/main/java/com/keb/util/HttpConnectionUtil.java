34e315e2ea14985dc4865faff26253bcf9115f88
Eunbyul <iks1614@gmail.com>
2020-04-23 11:54:04 +0900

    HttpConnection 위메프 사이트 대응

--- src/main/java/com/keb/util/HttpConnectionUtil.java
+++ src/main/java/com/keb/util/HttpConnectionUtil.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Optional;
+import java.util.zip.GZIPInputStream;
 
 public class HttpConnectionUtil {
 	public static String httpConnection(String targetUrl) {
@@ -22,8 +24,15 @@ public class HttpConnectionUtil {
             conn.setRequestProperty("Accept", "application/json");
             conn.setRequestMethod("GET");
             conn.connect();
-     
-            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            
+            System.out.println(conn.getHeaderFields());
+
+            if(Optional.ofNullable(conn.getHeaderField("Content-Encoding")).orElse("").contains("gzip")) {
+            	br = new BufferedReader(new InputStreamReader(new GZIPInputStream(conn.getInputStream()))); //wemakeprice 사이트 대응
+            } else {
+            	br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            }
+            
             sb = new StringBuffer();
             while ((line = br.readLine()) != null) {
                 sb.append(line);
